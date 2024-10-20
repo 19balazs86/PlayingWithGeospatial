4a979c1568aa4c52d286d3f032713643a0c79aff
19balazs86 <19balazs86@gmail.com>
2024-10-20 18:35:03 +0200

    Add a comment about using MS SQL DB

--- GeospatialWeb/Services/EntityFramework/ApplicationDbContext.cs
+++ GeospatialWeb/Services/EntityFramework/ApplicationDbContext.cs
@@ -13,6 +13,14 @@ public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(o
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        /* Some notes using MS SQL
+         * - I could not add indexes with HasMethod("SPATIAL")
+         * - But I was able to add the indexes using SSMS by selecting the field, a new button for Spatial indexes appeared
+         * - Only supports the GEOGRAPHY type and does not have specialized types for Polygon or Point
+         * - Instead of poi.Location.CoveredBy, you need to use Within | GeoFence.Covers -> GeoFence.Contains
+         * - Example: SELECT geography::Point(47.4882, 19.1043, 4326).STDistance(geography::Point(46.2485, 20.1517, 4326))
+        */
+
         // If you are using EF 6.0, you need to make sure that the PostGIS extension is installed in your database (later versions do this automatically)
         // modelBuilder.HasPostgresExtension("postgis");
 
